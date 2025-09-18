dcd5a89eebb19c0f7cd1c6e95ff734a4618f23e1
Aaron Rodriguez <69766780+AaronERodriguez@users.noreply.github.com>
2025-09-18 13:31:30 -0600

    added title

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { ModeToggle } from "@/components/toggle-theme";
 import Piano from "./piano";
 
 export default function Home() {
   return (
     <>
-      <nav>
-        <ModeToggle />
+      <nav className="w-full p-4 border-b border-b-slate-200 dark:border-b-slate-700 mb-4 bg-primary text-primary-foreground text-2xl text-center font-bold italic">
+        <h1>Piano App</h1>
       </nav>
       <div className="flex flex-col items-center min-h-screen p-4">
         <Piano />
